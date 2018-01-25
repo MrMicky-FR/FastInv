b7a45afd33b864dcd5b8c5ae78e5f784681faedb
MrMicky <themrmicky@gmail.com>
2018-01-25 13:44:51 +0100

    Initial commit

---
+++ src/main/java/fr/mrmicky/fastinv/ExemplePlugin.java
@@ -0,0 +1,45 @@
+package fr.mrmicky.fastinv;
+
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * An exemple plugin to show what FastInv can do
+ * 
+ * @author MrMicky
+ */
+public class ExemplePlugin extends JavaPlugin {
+
+	private Random r = new Random();
+
+	@Override
+	public void onEnable() {
+		FastInv.init(this);
+		
+		FastInv inv = new FastInv(54);
+		inv.onClose(event -> event.setCancelled(true));
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+ 			int[] int1 = new int[] { 0, 2, 4, 6, 8, 45, 47, 49, 51, 53 };
+			int[] int2 = new int[] { 1, 3, 5, 7, 46, 48, 50, 52 };
+			FastInv inv = new FastInv(54, "Custom Menu");
+			
+			inv.addItem(22, new ItemStack(Material.NAME_TAG), e -> inv.addItem(new ItemStack(Material.OBSIDIAN)))
+				.onUpdate(20, () -> inv.addItem(int1, new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) r.nextInt(15))))
+				.onUpdate(10, () -> inv.addItem(int2, new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) r.nextInt(15))))
+				.onClick(e -> p.sendMessage("You clicked on slot " + e.getSlot()))
+				.open(p);
+		}
+		return true;
+	}
+}
---
+++ src/main/java/fr/mrmicky/fastinv/FastInv.java
@@ -0,0 +1,447 @@
+package fr.mrmicky.fastinv;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+
+/**
+ * A fast API to easely create advanced GUI
+ * 
+ * @author MrMicky
+ * @version 1.0
+ */
+public class FastInv {
+
+	private static Plugin plugin;
+
+	public static void init(Plugin plugin) {
+		FastInv.plugin = plugin;
+	}
+
+	private Inventory inv;
+	private Listener listener;
+	private boolean destroy = false;
+	private boolean willDestroy = true;
+	private List<Player> viewers = new ArrayList<>();
+	private List<FastInvCloseListener> menuListeners = new ArrayList<>();
+	private List<FastInvClickListener> clickListeners = new ArrayList<>();
+	private HashMap<Integer, FastInvClickListener> itemListeners = new HashMap<>();
+	private List<BukkitTask> tasks = new ArrayList<>();
+
+	/**
+	 * Create a new FastInv with a custom size
+	 * 
+	 * @param size
+	 *            The size of the inventory
+	 */
+	public FastInv(int size) {
+		this(Bukkit.createInventory(null, size));
+	}
+
+	/**
+	 * Create a new FastInv with a custom size and title
+	 * 
+	 * @param size
+	 *            The size of the inventory
+	 * @param title
+	 *            The title of the inventory
+	 */
+	public FastInv(int size, String title) {
+		this(Bukkit.createInventory(null, size, title));
+	}
+
+	/**
+	 * Create a new FastInv with a custom type
+	 * 
+	 * @param size
+	 *            The type of the inventory
+	 */
+	public FastInv(InventoryType type) {
+		this(Bukkit.createInventory(null, type));
+	}
+
+	/**
+	 * Create a new FastInv with a custom type and title
+	 * 
+	 * @param type
+	 *            The type of the inventory
+	 * @param title
+	 *            The title of the inventory
+	 */
+	public FastInv(InventoryType type, String title) {
+		this(Bukkit.createInventory(null, type, title));
+	}
+
+	private FastInv(Inventory inv) {
+		checkDestroy();
+		if (this.inv != null) {
+			throw new IllegalStateException("Inventory is already init");
+		}
+
+		this.inv = inv;
+
+		registerListener();
+	}
+
+	/**
+	 * Add an item to the inventory
+	 * 
+	 * @param item
+	 *            The item to add
+	 * @return the FastInv build
+	 */
+	public FastInv addItem(ItemStack item) {
+		return addItem(item, null);
+	}
+
+	/**
+	 * Add an item to the inventory with a {@link FastInvClickListener} to
+	 * handle click
+	 * 
+	 * @param item
+	 *            The item to add
+	 * @param listener
+	 *            The {@link FastInvClickListener} for the item
+	 * @return the FastInv build
+	 */
+	public FastInv addItem(ItemStack item, FastInvClickListener listener) {
+		int slot = inv.firstEmpty();
+		if (slot != -1) {
+			return addItem(slot, item, listener);
+		}
+		return this;
+	}
+
+	/**
+	 * Add an item to the inventory on a specific slot
+	 * 
+	 * @param slot
+	 *            The slot of the item
+	 * @param item
+	 *            The item to add
+	 * @return the FastInv build
+	 */
+	public FastInv addItem(int slot, ItemStack item) {
+		return addItem(slot, item, null);
+	}
+
+	/**
+	 * Add an item to the inventory on specific slot with a
+	 * {@link FastInvClickListener} to handle click
+	 * 
+	 * @param slot
+	 *            The slot of the item
+	 * @param item
+	 *            The item to add
+	 * @param listener
+	 *            The {@link FastInvClickListener} for the item
+	 * @return the FastInv builder
+	 */
+	public FastInv addItem(int slot, ItemStack item, FastInvClickListener listener) {
+		checkDestroy();
+
+		inv.setItem(slot, item);
+		if (listener != null) {
+			itemListeners.put(slot, listener);
+		} else {
+			itemListeners.remove(slot);
+		}
+		return this;
+	}
+
+	/**
+	 * Add an item to the inventory on a multiples slots
+	 * 
+	 * @param slots
+	 *            The slot of the item
+	 * @param item
+	 *            The item to add
+	 * @return the FastInv build
+	 */
+	public FastInv addItem(int[] slots, ItemStack item) {
+		return addItem(slots, item, null);
+	}
+
+	/**
+	 * Add an item to the inventory on multiples slots with a
+	 * {@link FastInvClickListener} to handle click
+	 * 
+	 * @param slots
+	 *            The slots of the item
+	 * @param item
+	 *            The item to add
+	 * @param listener
+	 *            The {@link FastInvClickListener} for the item
+	 * @return the FastInv builder
+	 */
+	public FastInv addItem(int[] slots, ItemStack item, FastInvClickListener listener) {
+		for (int slot : slots) {
+			addItem(slot, item, listener);
+		}
+		return this;
+	}
+
+	/**
+	 * Add a {@link FastInvCloseListener} to listen on inventory close
+	 * 
+	 * @param listener
+	 *            The {@link FastInvCloseListener} to add
+	 * @return the FastInv builder
+	 */
+	public FastInv onClose(FastInvCloseListener listener) {
+		checkDestroy();
+
+		menuListeners.add(listener);
+		return this;
+	}
+
+	/**
+	 * Add a {@link FastInvClickListener} to listen on inventory click
+	 * 
+	 * @param listener
+	 *            The {@link FastInvClickListener} to add
+	 * @return the FastInv builder
+	 */
+	public FastInv onClick(FastInvClickListener listener) {
+		checkDestroy();
+
+		clickListeners.add(listener);
+		return this;
+	}
+
+	public FastInv onUpdate(int delay, Runnable runnable) {
+		checkDestroy();
+
+		tasks.add(Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, delay));
+		return this;
+	}
+
+	/**
+	 * Open the menu to players
+	 * 
+	 * @param players
+	 *            The players to open the menu
+	 * @return the FastInv builder
+	 */
+	public FastInv open(Player... players) {
+		checkDestroy();
+
+		for (Player p : players) {
+			viewers.add(p);
+			p.openInventory(inv);
+		}
+		return this;
+	}
+
+	public Inventory getInventory() {
+		return inv;
+	}
+
+	public boolean isDestroy() {
+		return this.destroy;
+	}
+
+	public List<Player> getViewers() {
+		return this.viewers;
+	}
+
+	private void checkDestroy() {
+		if (destroy) {
+			throw new IllegalStateException("This inv is destroyed");
+		}
+	}
+
+	/**
+	 * Destory this FastInv (cancel the tasks and unregister the listners)
+	 */
+	public void destroy() {
+		if (destroy) {
+			return;
+		}
+		new ArrayList<>(viewers).forEach(p -> p.closeInventory());
+		HandlerList.unregisterAll(listener);
+		tasks.forEach(t -> t.cancel());
+		destroy = true;
+	}
+
+	/**
+	 * Set if the menu will be destory when all viewers close it
+	 * 
+	 * @param destroy
+	 *            Set if the menu will destory
+	 * @return the FastInv builder
+	 */
+	public FastInv setWillDestroy(boolean destroy) {
+		this.willDestroy = destroy;
+		return this;
+	}
+
+	private void registerListener() {
+		listener = new Listener() {
+
+			@EventHandler
+			public void onOpen(InventoryOpenEvent e) {
+				if (e.getInventory().equals(inv) && e.getPlayer() instanceof Player) {
+					Player p = (Player) e.getPlayer();
+					checkDestroy();
+					if (!viewers.contains(p)) {
+						viewers.add(p);
+					}
+				}
+			}
+
+			@EventHandler
+			public void onClick(InventoryClickEvent e) {
+				if (e.getInventory().equals(inv) && e.getCurrentItem() != null && e.getWhoClicked() instanceof Player) {
+					e.setCancelled(true);
+					int slot = e.getRawSlot();
+					FastInvClickEvent ev = new FastInvClickEvent((Player) e.getWhoClicked(), FastInv.this, slot,
+							e.getCurrentItem(), e.getAction(), e.getClick());
+					if (itemListeners.containsKey(slot)) {
+						itemListeners.get(slot).onClick(ev);
+					}
+					clickListeners.forEach(l -> l.onClick(ev));
+				}
+			}
+
+			@EventHandler
+			public void onClose(InventoryCloseEvent e) {
+				if (e.getInventory().equals(inv) && e.getPlayer() instanceof Player) {
+					Player p = (Player) e.getPlayer();
+
+					FastInvCloseEvent ev = new FastInvCloseEvent(p, FastInv.this, false);
+					menuListeners.forEach(l -> l.onClose(ev));
+
+					if (ev.isCancelled()) {
+						// delay to prevent big errors
+						Bukkit.getScheduler().runTask(plugin, () -> p.openInventory(inv));
+					} else {
+						viewers.remove(p);
+						if (viewers.isEmpty() && willDestroy) {
+							destroy();
+						}
+					}
+				}
+			}
+
+			@EventHandler
+			public void onQuit(PlayerQuitEvent e) {
+				Player p = e.getPlayer();
+				if (viewers.remove(p)) {
+					menuListeners.forEach(l -> l.onClose(new FastInvCloseEvent(p, FastInv.this, false)));
+					if (viewers.isEmpty() && willDestroy) {
+						destroy();
+					}
+				}
+			}
+
+			@EventHandler
+			public void onDisable(PluginDisableEvent e) {
+				if (e.getPlugin().equals(plugin)) {
+					destroy();
+				}
+			}
+		};
+
+		Bukkit.getPluginManager().registerEvents(listener, plugin);
+	}
+
+	class FastInvClickEvent {
+
+		private Player player;
+		private FastInv inventory;
+		private int slot;
+		private ItemStack item;
+		private InventoryAction action;
+		private ClickType clickType;
+
+		public FastInvClickEvent(Player player, FastInv inventory, int slot, ItemStack item, InventoryAction action,
+				ClickType clickType) {
+			this.player = player;
+			this.inventory = inventory;
+			this.slot = slot;
+			this.item = item;
+			this.action = action;
+			this.clickType = clickType;
+		}
+
+		public Player getPlayer() {
+			return this.player;
+		}
+
+		public FastInv getInventory() {
+			return this.inventory;
+		}
+
+		public int getSlot() {
+			return this.slot;
+		}
+
+		public ItemStack getItem() {
+			return this.item;
+		}
+
+		public InventoryAction getAction() {
+			return this.action;
+		}
+
+		public ClickType getClickType() {
+			return this.clickType;
+		}
+	}
+
+	class FastInvCloseEvent {
+
+		private Player player;
+		private FastInv inventory;
+		private boolean cancelled;
+
+		public FastInvCloseEvent(Player player, FastInv inventory, boolean cancelled) {
+			this.player = player;
+			this.inventory = inventory;
+			this.cancelled = cancelled;
+		}
+
+		public Player getPlayer() {
+			return this.player;
+		}
+
+		public FastInv getInventory() {
+			return this.inventory;
+		}
+
+		public boolean isCancelled() {
+			return this.cancelled;
+		}
+
+		public void setCancelled(boolean cancelled) {
+			this.cancelled = cancelled;
+		}
+	}
+
+	public interface FastInvClickListener {
+		public void onClick(FastInvClickEvent event);
+	}
+
+	public interface FastInvCloseListener {
+		public void onClose(FastInvCloseEvent event);
+	}
+}
