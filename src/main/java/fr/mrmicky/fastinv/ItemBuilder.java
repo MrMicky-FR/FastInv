4da93f61a3df20c26cda7ba7dc33cdf0073c64d6
HSGamer <huynhqtienvtag@gmail.com>
2023-01-05 11:55:43 +0700

    Slightly complicated ItemBuilder
    log edit actions to a list and execute them on build

--- src/main/java/fr/mrmicky/fastinv/ItemBuilder.java
+++ src/main/java/fr/mrmicky/fastinv/ItemBuilder.java
@@ -31,10 +31,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Consumer;
 
 /**
@@ -44,76 +41,80 @@ import java.util.function.Consumer;
  */
 public class ItemBuilder {
 
-    private final ItemStack item;
-    private final ItemMeta meta;
+    private final ItemStack initialItem;
+    private final List<Consumer<ItemStack>> actions;
 
     public ItemBuilder(Material material) {
         this(new ItemStack(material));
     }
 
     public ItemBuilder(ItemStack item) {
-        this.item = Objects.requireNonNull(item, "item");
-        this.meta = item.getItemMeta();
+        this.initialItem = Objects.requireNonNull(item, "item");
+        this.actions = new LinkedList<>();
+    }
 
-        if (this.meta == null) {
-            throw new IllegalArgumentException("The type " + item.getType() + " doesn't support item meta");
-        }
+    public ItemBuilder edit(Consumer<ItemStack> action) {
+        actions.add(action);
+        return this;
+    }
+
+    public ItemBuilder meta(Consumer<ItemMeta> metaConsumer) {
+        return edit(item -> {
+            ItemMeta meta = item.getItemMeta();
+            if (meta != null) {
+                metaConsumer.accept(meta);
+                item.setItemMeta(meta);
+            }
+        });
+    }
+
+    public <T extends ItemMeta> ItemBuilder meta(Class<T> metaClass, Consumer<T> metaConsumer) {
+        return meta(meta -> {
+            if (metaClass.isInstance(meta)) {
+                metaConsumer.accept(metaClass.cast(meta));
+            }
+        });
     }
 
     public ItemBuilder type(Material material) {
-        this.item.setType(material);
-        return this;
+        return edit(item -> item.setType(material));
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemBuilder durability(short durability) {
+        return edit(item -> item.setDurability(durability));
     }
 
     public ItemBuilder data(int data) {
         return durability((short) data);
     }
 
-    @SuppressWarnings("deprecation")
-    public ItemBuilder durability(short durability) {
-        this.item.setDurability(durability);
-        return this;
+    public ItemBuilder amount(int amount) {
+        return edit(item -> item.setAmount(amount));
     }
 
-    public ItemBuilder amount(int amount) {
-        this.item.setAmount(amount);
-        return this;
+    public ItemBuilder enchant(Enchantment enchantment, int level) {
+        return meta(meta -> meta.addEnchant(enchantment, level, true));
     }
 
     public ItemBuilder enchant(Enchantment enchantment) {
         return enchant(enchantment, 1);
     }
 
-    public ItemBuilder enchant(Enchantment enchantment, int level) {
-        this.meta.addEnchant(enchantment, level, true);
-        return this;
-    }
-
     public ItemBuilder removeEnchant(Enchantment enchantment) {
-        this.meta.removeEnchant(enchantment);
-        return this;
+        return meta(meta -> meta.removeEnchant(enchantment));
     }
 
     public ItemBuilder removeEnchants() {
-        this.meta.getEnchants().keySet().forEach(this.meta::removeEnchant);
-        return this;
-    }
-
-    public ItemBuilder meta(Consumer<ItemMeta> metaConsumer) {
-        metaConsumer.accept(this.meta);
-        return this;
-    }
-
-    public <T extends ItemMeta> ItemBuilder meta(Class<T> metaClass, Consumer<T> metaConsumer) {
-        if (metaClass.isInstance(this.meta)) {
-            metaConsumer.accept(metaClass.cast(this.meta));
-        }
-        return this;
+        return meta(meta -> meta.getEnchants().keySet().forEach(meta::removeEnchant));
     }
 
     public ItemBuilder name(String name) {
-        this.meta.setDisplayName(name);
-        return this;
+        return meta(meta -> meta.setDisplayName(name));
+    }
+
+    public ItemBuilder lore(List<String> lore) {
+        return meta(meta -> meta.setLore(lore));
     }
 
     public ItemBuilder lore(String lore) {
@@ -124,20 +125,16 @@ public class ItemBuilder {
         return lore(Arrays.asList(lore));
     }
 
-    public ItemBuilder lore(List<String> lore) {
-        this.meta.setLore(lore);
-        return this;
-    }
-
     public ItemBuilder addLore(String line) {
-        List<String> lore = this.meta.getLore();
-
-        if (lore == null) {
-            return lore(line);
-        }
-
-        lore.add(line);
-        return lore(lore);
+        return meta(meta -> {
+            List<String> lore = meta.getLore();
+            if (lore == null) {
+                lore = Collections.singletonList(line);
+            } else {
+                lore.add(line);
+            }
+            meta.setLore(lore);
+        });
     }
 
     public ItemBuilder addLore(String... lines) {
@@ -145,19 +142,19 @@ public class ItemBuilder {
     }
 
     public ItemBuilder addLore(List<String> lines) {
-        List<String> lore = this.meta.getLore();
-
-        if (lore == null) {
-            return lore(lines);
-        }
-
-        lore.addAll(lines);
-        return lore(lore);
+        return meta(meta -> {
+            List<String> lore = meta.getLore();
+            if (lore == null) {
+                lore = lines;
+            } else {
+                lore.addAll(lines);
+            }
+            meta.setLore(lore);
+        });
     }
 
     public ItemBuilder flags(ItemFlag... flags) {
-        this.meta.addItemFlags(flags);
-        return this;
+        return meta(meta -> meta.addItemFlags(flags));
     }
 
     public ItemBuilder flags() {
@@ -165,8 +162,7 @@ public class ItemBuilder {
     }
 
     public ItemBuilder removeFlags(ItemFlag... flags) {
-        this.meta.removeItemFlags(flags);
-        return this;
+        return meta(meta -> meta.removeItemFlags(flags));
     }
 
     public ItemBuilder removeFlags() {
@@ -178,7 +174,8 @@ public class ItemBuilder {
     }
 
     public ItemStack build() {
-        this.item.setItemMeta(this.meta);
-        return this.item;
+        ItemStack item = initialItem.clone();
+        actions.forEach(a -> a.accept(item));
+        return item;
     }
 }
