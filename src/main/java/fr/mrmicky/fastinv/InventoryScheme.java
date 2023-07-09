79e98ac3a68f53b4886f4b17a94cf6339f5493df
MrMicky <git@mrmicky.fr>
2023-07-09 22:13:43 +0200

    Small refactoring

--- src/main/java/fr/mrmicky/fastinv/InventoryScheme.java
+++ src/main/java/fr/mrmicky/fastinv/InventoryScheme.java
@@ -3,81 +3,104 @@ package fr.mrmicky.fastinv;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 public class InventoryScheme {
 
-    private final LinkedList<String> maskList = new LinkedList<>();
-    private final HashMap<Character, Pair<ItemStack, Consumer<InventoryClickEvent>>> schemeItems = new HashMap<>();
-
-    private static final class Pair<A, B> {
-        public final A fst;
-        public final B snd;
-
-        public Pair(A fst, B snd) {
-            this.fst = fst;
-            this.snd = snd;
-        }
-    }
+    private final List<String> masks = new ArrayList<>();
+    private final Map<Character, ItemStack> items = new HashMap<>();
+    private final Map<Character, Consumer<InventoryClickEvent>> handlers = new HashMap<>();
 
     /**
-     * Add a mask to the corresponding FastInv including all sort of characters.
-     * For Example: "110101011"
+     * Add a mask to this scheme including all sort of characters.
+     * For example: "110101011"
      *
-     * @param mask not nullable string of the mask to add.
+     * @param mask a 9 characters mask
+     * @return this scheme instance
      */
     public InventoryScheme mask(String mask) {
         Objects.requireNonNull(mask);
-        maskList.add(mask.length() > 9 ? mask.substring(0, 10) : mask);
+        this.masks.add(mask.length() > 9 ? mask.substring(0, 10) : mask);
+
         return this;
     }
 
     /**
-     * Bind character to the corresponding itemstack in the inventory.
-     * @param character not nullable character. Example: '4'
-     * @param item not nullable itemstack to be binded with the character
-     * @param consumer nullable consumer for the item
+     * Add multiples masks to this scheme including all sort of characters.
+     * For example: "111111111", "110101011", "111111111"
+     *
+     * @param masks multiple 9-characters masks
+     * @return this scheme instance
      */
-    public InventoryScheme bindItem(Character character, ItemStack item, Consumer<InventoryClickEvent> consumer) {
-        Objects.requireNonNull(character);
-        Objects.requireNonNull(item);
-        schemeItems.put(character, new Pair<>(item, consumer));
+    public InventoryScheme masks(String... masks) {
+        for (String mask : Objects.requireNonNull(masks)) {
+            mask(mask);
+        }
         return this;
+    }
+
+    /**
+     * Bind character to the corresponding item in the inventory.
+     *
+     * @param character the associated character in the mask
+     * @param item      the item to use for this character
+     * @param handler   consumer for the item
+     * @return this scheme instance
+     */
+    public InventoryScheme bindItem(char character, ItemStack item, Consumer<InventoryClickEvent> handler) {
+        this.items.put(character, Objects.requireNonNull(item));
+
+        if (handler != null) {
+            this.handlers.put(character, handler);
+        }
+        return this;
+    }
+
+    /**
+     * Bind character to the corresponding item in the inventory.
+     *
+     * @param character the associated character in the mask
+     * @param item      the item to use for this character
+     * @return this scheme instance
+     */
+    public InventoryScheme bindItem(char character, ItemStack item) {
+        return this.bindItem(character, item, null);
     }
 
     /**
      * Unbind any item from this character.
-     * @param character not nullable character
+     *
+     * @param character the character to unbind
+     * @return this scheme instance
      */
-    public InventoryScheme unbindItem(Character character) {
-        Objects.requireNonNull(character);
-        schemeItems.remove(character);
+    public InventoryScheme unbindItem(char character) {
+        this.items.remove(character);
+        this.handlers.remove(character);
         return this;
     }
 
     /**
-     * Apply the current inventory scheme to the FastInv object.
-     * @param fastInv not nullable fastinv that will be applied this scheme.
+     * Apply the current inventory scheme to the FastInv instance.
+     *
+     * @param inv the FastInv instance to apply this scheme to
      */
-    public void apply(FastInv fastInv) {
-        Objects.requireNonNull(fastInv);
-        int slot = 0;
-        for(String mask : maskList) {
-            for(int i = 0; i <= 8; i++) {
-                try {
-                    final Character character = mask.charAt(i);
-                    final Pair<ItemStack, Consumer<InventoryClickEvent>> pair = schemeItems.get(character);
-                    if(pair != null) {
-                        fastInv.setItem(slot, pair.fst, pair.snd);
-                    }
-                } catch (IndexOutOfBoundsException ignored) {
-                    // mean mask's length is greater than 9 characters. Shouldn't happen, can be ignored.
-                }
+    public void apply(FastInv inv) {
+        for (int line = 0; line < this.masks.size(); line++) {
+            String mask = this.masks.get(line);
 
-                slot++;
+            for (int slot = 0; slot < mask.length(); slot++) {
+                char c = mask.charAt(slot);
+                ItemStack item = this.items.get(c);
+                Consumer<InventoryClickEvent> handler = this.handlers.get(c);
+
+                if (item != null) {
+                    inv.setItem(9 * line + slot, item, handler);
+                }
             }
         }
     }
